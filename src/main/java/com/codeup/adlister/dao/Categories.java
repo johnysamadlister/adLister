08046e7d442f6d9ac98ca0c6988ef0442088e1aa
johnnyruiz <ruiz.johnny.p@gmail.com>
2019-06-18 15:26:14 -0500

    Some of the doas still need work

---
+++ src/main/java/com/codeup/adlister/dao/Categories.java
@@ -0,0 +1,9 @@
+package com.codeup.adlister.dao;
+
+import com.codeup.adlister.models.Ad;
+import com.codeup.adlister.models.Category;
+
+
+public interface Categories {
+    Category findByCategory();
+}
